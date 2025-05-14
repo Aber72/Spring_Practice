b883abfea14baabdf6fafa5e5c04b6d12ffa87d3
abhishek <abh9204@gmail.com>
2025-05-14 21:10:42 +0530

    spring core - create object of spring ioc using applicationcontext class and call the bean to achieve dependency injection

--- springcore/src/main/java/com/springcore/Application.java
+++ springcore/src/main/java/com/springcore/Application.java
@@ -1,9 +1,15 @@
 package com.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application
 {
 	public static void main( String[] args )
 	{
 		System.out.println("Hello world");
+		ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml");
+		Student student1 =(Student) context.getBean("student");
+		System.out.println(student1);
 	}
 }
