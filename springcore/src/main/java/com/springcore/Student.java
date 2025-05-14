0193f1b34a051817bea993229ac946a0781a05a3
abhishek <abh9204@gmail.com>
2025-05-14 20:55:10 +0530

    spring core - create student class to call directly with IOC container from Application class without creating object of student class

---
+++ springcore/src/main/java/com/springcore/Student.java
@@ -0,0 +1,42 @@
+package com.springcore;
+
+public class Student {
+	int id;
+	String name;
+	String adress;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public Student(int id, String name, String adress) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.adress = adress;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", adress=" + adress + "]";
+	}
+	
+
+}
