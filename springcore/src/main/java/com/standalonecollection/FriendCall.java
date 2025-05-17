69172a1d3b8f730ad6e07dad58cbbbce05275233
abhishek <abh9204@gmail.com>
2025-05-17 15:16:00 +0530

    springcore- standalone collection - testing

--- springcore/src/main/java/com/standalonecollection/FriendCall.java
+++ springcore/src/main/java/com/standalonecollection/FriendCall.java
@@ -8,9 +8,11 @@ public class FriendCall {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("standalonecln.xml");
 		
-		Bestfriends bestfriends= (Bestfriends) context.getBean("frd");
+		Bestfriends bestfriends= (Bestfriends) context.getBean("frd1");
+		Bestfriends bestfriends1= (Bestfriends) context.getBean("frd");
 		
 		System.out.println(bestfriends);
+		System.out.println(bestfriends1);
 		
 	}
 
